98f0b6fbbeb11c8e91ec68f056b640c54b02f116
sandra <nyamweasandra@gmail.com>
2023-05-22 15:11:46 +0300

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word.downcase
+  end
+
+  def match(anagrams)
+    anagrams.select { |anagram| anagram.downcase.chars.sort == word.chars.sort }
+  end
+end
+
+
+listen = Anagram.new("listen")
+matches = listen.match(%w[enlists google inlets banana])
+puts matches.inspect
